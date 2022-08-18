41b82767c771abed73d554741dc7f849cb7a719a
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-08-18 09:29:39 -0400

    completed assignment

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let num2 = 2;
 let multiply = (num1 * num2)
 
 // generates a random integer greater than 0
+// math.floor- rounds the number down
+// Math.round()-rounds the number either up or down
 let random = (Math.floor(Math.random() * 10) + 1);
 
 //is an equation that calculates a remainder that is equal to 4
@@ -16,4 +18,5 @@ let mod = (num3 % num4)
 let x = 5;
 let y = 6;
 let z = 20;
+// Math.max()-accepts the no of arguments and return highest lowest constituent
 let max = (Math.max(x,y,z))
